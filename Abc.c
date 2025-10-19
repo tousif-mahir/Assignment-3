69c5c12f7ea5d077043fa0ab8d10759ec88db9d6
tousif-mahir <almahir457@gmail.com>
2025-10-20 01:30:11 +0600

    Renamed variables for clarity

--- Abc.c
+++ Abc.c
@@ -1,41 +1,39 @@
-#include <stdio.h>
-
-int main() {
-    int a[3][3] = {{50,60,70},{80,90,100},{30,40,50}};
-    char n[3][10] = {"Ali","Bob","Cat"};
-    int i, j, s;
+#include<stdio.h>
+int main(){
+    int mark[3][3]={{50,60,70},{80,90,100},{30,40,50}};
+    char name[3][10]={"Ali","Bob","Cat"};
+    int i,j,sum;
     float avg;
-
-    for(i = 0; i < 3; i++) {
-        s = 0;
-        for(j = 0; j < 3; j++) {
-            s += a[i][j];
+    for(i=0;i<3;i++){
+        sum=0;
+        for(j=0;j<3;j++){
+            sum+=mark[i][j];
         }
-        avg = s / 3.0;
-
-        if(avg >= 80)
-            printf("%s Grade A avg=%.2f\n", n[i], avg);
-        else if(avg >= 60)
-            printf("%s Grade B avg=%.2f\n", n[i], avg);
-        else if(avg >= 40)
-            printf("%s Grade C avg=%.2f\n", n[i], avg);
-        else
-            printf("%s Fail avg=%.2f\n", n[i], avg);
-    }
-
-    int max = 0, pos = 0;
-    for(i = 0; i < 3; i++) {
-        s = 0;
-        for(j = 0; j < 3; j++) {
-            s += a[i][j];
+        avg=sum/3.0;
+        if(avg>=80){
+            printf("%s Grade A avg=%.2f\n",name[i],avg);
         }
-        if(s > max) {
-            max = s;
-            pos = i;
+        else if(avg>=60){
+            printf("%s Grade B avg=%.2f\n",name[i],avg);
+        }
+        else if(avg>=40){
+            printf("%s Grade C avg=%.2f\n",name[i],avg);
+        } 
+        else{
+            printf("%s Fail avg=%.2f\n",name[i],avg);
         }
     }
-
-    printf("Topper: %s with total %d\n", n[pos], max);
-
+    int max=0,pos=0;
+    for(i=0;i<3;i++){
+        sum=0;
+        for(j=0;j<3;j++){
+            sum+=mark[i][j];
+        }
+        if(sum>max){
+            max=sum;
+            pos=i;
+        }
+    }
+    printf("Topper: %s with total %d\n",name[pos],max);
     return 0;
 }
